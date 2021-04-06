7809b59f72f629a0d9d2cc0d3b59360771205355
IsabelDuran <isabelduranchumillas@gmail.com>
2021-04-06 13:35:37 +0200

    Using a bit of pattern matching for the Json body request

--- app/controllers/ProjectController.scala
+++ app/controllers/ProjectController.scala
@@ -2,8 +2,7 @@ package controllers
 
 import daos.ProjectDAO
 import models.Project
-
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json._
 import play.api.mvc._
 
 import javax.inject.Inject
@@ -27,12 +26,22 @@ class ProjectController @Inject()(val controllerComponents: ControllerComponents
       .map(project => Ok(Json.toJson(project)))
   }
 
-  def addProject = Action.async { implicit request =>
-    val bodyParam = (request.body.asJson.get \ "name").as[String]
-    projectDAO
-      .add(Project(name = bodyParam))
-      .map(_ => Ok(Json.toJson("New project created")))
-  }
+  def addProject: Action[AnyContent] = Action.async { implicit request =>
+    val jsValue = request.body.asJson.get
+    jsValue.toString()
+    jsValue match {
+      case JsString(value) =>  projectDAO
+                              .add(Project(name = value))
+                              .map(_ => Ok(Json.toJson(s"New project created with name $value")))
+      case JsNumber(value)  => projectDAO
+                              .add(Project(name = value.toString()))
+                              .map(_ => Ok(Json.toJson(s"New project created with name ${value.toString}")))
 
+      case JsObject(value)  => projectDAO.add(Project(name = value.toList.head._2.toString())).map(_ => Ok(s"New project created with name ${value.toList.head._2}"))
+
+      case _                => Future(BadRequest)
+    }
+
+  }
 
 }
